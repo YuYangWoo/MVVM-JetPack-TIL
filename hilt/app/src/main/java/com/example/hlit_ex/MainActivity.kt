a3c7d0a3313288c020ba3ef84a1f136a5fd1ddd7
유양우(Yangwoo Yu)/App개발팀/11ST <yangwoo.yu@sk.com>
2022-04-05 17:04:24 +0900

    feat: 필요한 Library import & Init Project

---
+++ hilt/app/src/main/java/com/example/hlit_ex/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.hlit_ex
+
+import com.example.hlit_ex.databinding.ActivityMainBinding
+import com.example.library.navigation.NavigationActivity
+
+class MainActivity : NavigationActivity<ActivityMainBinding>(R.layout.activity_main, R.id.fragmentContainerView) {
+
+}
---
+++ hilt/app/src/main/java/com/example/hlit_ex/MainFragment.kt
@@ -0,0 +1,13 @@
+package com.example.hlit_ex
+
+import com.example.hlit_ex.databinding.FragmentMainBinding
+import com.example.library.base.BaseFragment
+import com.example.library.binding.BindingFragment
+
+class MainFragment : BindingFragment<FragmentMainBinding>(R.layout.fragment_main) {
+    override fun init() {
+        super.init()
+
+    }
+
+}
