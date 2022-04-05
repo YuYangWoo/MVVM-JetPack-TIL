ecf080cedeb6cdbea2b86e01e4b2e9b5274d9b7f
유양우(Yangwoo Yu)/App개발팀/11ST <yangwoo.yu@sk.com>
2022-04-05 21:39:27 +0900

    refactor: directory change

--- hilt/app/src/main/java/com/example/hlit_ex/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hlit_ex
-
-import com.example.hlit_ex.databinding.ActivityMainBinding
-import com.example.library.navigation.NavigationActivity
-
-class MainActivity : NavigationActivity<ActivityMainBinding>(R.layout.activity_main, R.id.fragmentContainerView) {
-
-}
