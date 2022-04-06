ce939535100f608b3217ba6ce90488b1117a8d78
유양우(Yangwoo Yu)/App개발팀/11ST <yangwoo.yu@sk.com>
2022-04-06 13:22:53 +0900

    feat: ProgressDialog & ViewModel Implement through activityViewModel

---
+++ hilt/app/src/main/java/com/example/hlit_ex/ui/view/dialog/SummonerInputDialog.kt
@@ -0,0 +1,66 @@
+package com.example.hlit_ex.ui.view.dialog
+
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.example.hlit_ex.R
+import com.example.hlit_ex.data.model.response.LeagueResponse
+import com.example.hlit_ex.data.model.response.SummonerResponse
+import com.example.hlit_ex.databinding.DialogSummonerInputBinding
+import com.example.hlit_ex.ui.view.fragment.MainFragment
+import com.example.hlit_ex.ui.viewmodel.MainViewModel
+import com.example.hlit_ex.util.Resource
+import com.example.library.binding.BindingDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SummonerInputDialog :
+    BindingDialogFragment<DialogSummonerInputBinding>(R.layout.dialog_summoner_input) {
+    private val mainViewModel by activityViewModels<MainViewModel>()
+    private var summonerResponse: SummonerResponse? = null
+    private val progressDialog: ProgressDialog by lazy { ProgressDialog(requireContext()) }
+
+    override fun init() {
+        super.init()
+        confirmBtn()
+        observerSummonerData()
+    }
+
+    private fun observerSummonerData() {
+        mainViewModel.summonerResponse.observe(viewLifecycleOwner, Observer { resource ->
+            when (resource.status) {
+                Resource.Status.SUCCESS -> {
+                    progressDialog.dismiss()
+                    summonerResponse = resource.data?.body() ?: return@Observer
+                    mainViewModel.requestLeagueInfo(summonerResponse!!.id)
+                    Log.d(TAG, "observerSummonerData: ${summonerResponse}")
+                    dismiss()
+                }
+                Resource.Status.LOADING -> {
+                    progressDialog.show()
+                }
+                Resource.Status.ERROR -> {
+                    Log.d(TAG, "init: 실패${resource.message}")
+                }
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requireContext().dialogFragmentResize(this@SummonerInputDialog, 0.8f, 0.25f)
+    }
+
+    private fun confirmBtn() {
+        binding.setOnOkClick {
+            mainViewModel.requestSummonerInfo(binding.nameInputLayout.editText?.text.toString())
+        }
+    }
+
+
+    companion object {
+        private const val TAG = "SummonerInputDialog"
+    }
+}
--- hilt/app/src/main/java/com/example/hlit_ex/ui/view/fragment/MainFragment.kt
+++ hilt/app/src/main/java/com/example/hlit_ex/ui/view/fragment/MainFragment.kt
@@ -1,8 +1,11 @@
 package com.example.hlit_ex.ui.view.fragment
 
 import android.util.Log
+import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.example.hlit_ex.R
 import com.example.hlit_ex.data.model.response.LeagueResponse
 import com.example.hlit_ex.data.model.response.SummonerResponse
@@ -12,41 +15,22 @@ import com.example.hlit_ex.ui.viewmodel.MainViewModel
 import com.example.hlit_ex.util.Resource
 import com.example.library.binding.BindingFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainFragment : BindingFragment<FragmentMainBinding>(R.layout.fragment_main) {
-    private val mainViewModel by viewModels<MainViewModel>()
+    private val mainViewModel by activityViewModels<MainViewModel>()
     private var summonerResponse: SummonerResponse? = null
     private var leagueResponse: List<LeagueResponse>? = null
     private val progressDialog: ProgressDialog by lazy { ProgressDialog(requireContext()) }
+
     override fun init() {
         super.init()
-        requestSummonerInfo()
-        observerSummonerData()
         observerLeagueData()
-    }
-
-    private fun requestSummonerInfo() {
-        mainViewModel.requestSummonerInfo("누누와 우양유")
-    }
-
-    private fun observerSummonerData() {
-        mainViewModel.summonerResponse.observe(viewLifecycleOwner, Observer { resource ->
-            when (resource.status) {
-                Resource.Status.SUCCESS -> {
-                    progressDialog.dismiss()
-                    summonerResponse = resource.data?.body() ?: return@Observer
-                    mainViewModel.requestLeagueInfo(summonerResponse?.id ?: return@Observer)
-                    Log.d(TAG, "observerSummonerData: ${summonerResponse}")
-                }
-                Resource.Status.LOADING -> {
-                    progressDialog.show()
-                }
-                Resource.Status.ERROR -> {
-                    Log.d(TAG, "init: 실패${resource.message}")
-                }
-            }
-        })
+        addSummonerInfo()
     }
 
     private fun observerLeagueData() {
@@ -66,6 +50,12 @@ class MainFragment : BindingFragment<FragmentMainBinding>(R.layout.fragment_main
                 }
             }
         })
+    }
+
+   private fun addSummonerInfo() {
+       binding.btnSummonerAdd.setOnClickListener {
+           findNavController().navigate(R.id.action_mainFragment_to_summonerInputDialog)
+       }
     }
 
     companion object {
