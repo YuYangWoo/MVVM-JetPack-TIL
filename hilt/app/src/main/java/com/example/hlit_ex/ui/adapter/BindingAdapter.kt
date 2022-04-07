a87a09d66ed88f3da0283e623b089284dc7629aa
유양우(Yangwoo Yu)/App개발팀/11ST <yangwoo.yu@sk.com>
2022-04-07 11:10:33 +0900

    feat: BindingAdapter Implement

---
+++ hilt/app/src/main/java/com/example/hlit_ex/ui/adapter/BindingAdapter.kt
@@ -0,0 +1,17 @@
+package com.example.hlit_ex.ui.adapter
+
+import android.util.Log
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+object BindingAdapter {
+    @JvmStatic
+    @BindingAdapter("profileIcon")
+    fun setProfileIcon(imageView: ImageView, number: Int) {
+        Log.d("PASS", "setProfileIcon: ${number}")
+        Glide.with(imageView.context)
+            .load("https://ddragon.leagueoflegends.com/cdn/12.6.1/img/profileicon/${number}.png")
+            .into(imageView)
+    }
+}
