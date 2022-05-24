52a23e27884188e89f495ba4fbd76903cf984541
YuYangWoo <yuyw0712@naver.com>
2022-05-24 10:22:01 +0900

    feat: MainActivity Set up

--- DynamicRecyclerView/app/src/main/java/com/example/dynamicrecyclerview/MainActivity.kt
+++ DynamicRecyclerView/app/src/main/java/com/example/dynamicrecyclerview/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.dynamicrecyclerview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dynamicrecyclerview.databinding.ActivityMainBinding
+import com.example.library.navigation.NavigationActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : NavigationActivity<ActivityMainBinding>(R.layout.activity_main, R.id.fragmentContainerView) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
