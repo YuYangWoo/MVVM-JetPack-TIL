f0d90e54b5dc9895564b4e83aabb3838766cd5f9
YuYangWoo <yuyw0712@naver.com>
2022-05-24 10:25:50 +0900

    feat: Enter Second View through viewholder click event

---
+++ DynamicRecyclerView/app/src/main/java/com/example/dynamicrecyclerview/StudentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dynamicrecyclerview
+
+import com.example.dynamicrecyclerview.databinding.ActivityStudentBinding
+import com.example.library.binding.BindingActivity
+
+class StudentActivity : BindingActivity<ActivityStudentBinding>(R.layout.activity_student) {
+    override fun init() {
+        super.init()
+
+    }
+}
