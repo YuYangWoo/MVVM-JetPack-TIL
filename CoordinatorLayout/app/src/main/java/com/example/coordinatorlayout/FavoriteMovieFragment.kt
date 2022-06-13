4feec418df500702da2053ec6e3783fc578f9e2c
YuYangWoo <yuyw0712@naver.com>
2022-06-13 10:19:13 +0900

    feat: FavoriteMovieFragment setUp

---
+++ CoordinatorLayout/app/src/main/java/com/example/coordinatorlayout/FavoriteMovieFragment.kt
@@ -0,0 +1,7 @@
+package com.example.coordinatorlayout
+
+import com.example.coordinatorlayout.databinding.FragmentFavoriteMovieBinding
+import com.example.library.binding.BindingFragment
+
+class FavoriteMovieFragment : BindingFragment<FragmentFavoriteMovieBinding>(R.layout.fragment_favorite_movie) {
+}
