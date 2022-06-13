0b3993fd89dba0bf6fc504bea1fd23dd5e9d8333
YuYangWoo <yuyw0712@naver.com>
2022-06-13 09:47:19 +0900

    feat: MainActivity setUp

---
+++ CoordinatorLayout/app/src/main/java/com/example/coordinatorlayout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coordinatorlayout
+
+import com.example.coordinatorlayout.databinding.ActivityMainBinding
+import com.example.library.navigation.NavigationActivity
+
+class MainActivity : NavigationActivity<ActivityMainBinding>(R.layout.activity_main, R.id.fragmentContainerView)
